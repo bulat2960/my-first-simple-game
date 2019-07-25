e0fba05ddc7a67cb88f4b6cd74692c518694a92c
Bulat Gubaydullin <gubaydullin.bulat7@gmail.com>
2019-07-25 20:15:56 +0300

    Add signal to find character who taken the bonus

--- objects/bonus.h
+++ objects/bonus.h
@@ -1,11 +1,14 @@
 #ifndef BONUS_H
 #define BONUS_H
 
+#include "character.h"
 
 class Bonus
 {
 public:
     Bonus();
+signals:
+    void signalTakenBy(Character* character);
 };
 
 #endif // BONUS_H
