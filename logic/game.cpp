9d37dfae72c8231da97677f9b53eb0e1939b0498
Bulat Gubaydullin <gubaydullin.bulat7@gmail.com>
2019-07-25 21:24:26 +0300

    Add class game constructor realization

--- logic/game.cpp
+++ logic/game.cpp
@@ -1,6 +1,7 @@
 #include "game.h"
 
-Game::Game()
+Game::Game(int botsNumber, int bonusesNumber, int portalsNumber,
+           int mHeight, int mWidth, int sHeight, int sWidth)
 {
 
 }
--- logic/game.h
+++ logic/game.h
@@ -1,11 +1,34 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "battleexecutor.h"
+#include "bonusgenerator.h"
+#include "collisiondetector.h"
+
+#include "maze/maze.h"
+
+#include "objects/bonus.h"
+#include "objects/player.h"
+#include "objects/bot.h"
+#include "objects/portal.h"
+
 
 class Game
 {
+    Maze* maze;
+
+    Player* player;
+    QVector<Bot*> bots;
+    QVector<Bonus*> bonuses;
+    QVector<Portal*> portals;
+
+    BattleExecutor* battleExecutor;
+    CollisionDetector* collisionDetector;
+    BonusGenerator* bonusGenerator;
+
 public:
-    Game();
+    Game(int botsNumber, int bonusesNumber, int portalsNumber,
+         int mHeight, int mWidth, int sHeight, int sWidth);
 };
 
 #endif // GAME_H
