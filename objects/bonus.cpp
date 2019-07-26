97f40388ee46150f5b49eb41f493697cd9feb231
Bulat Gubaydullin <gubaydullin.bulat7@gmail.com>
2019-07-26 22:13:34 +0300

    Add bonus image painting

--- objects/bonus.cpp
+++ objects/bonus.cpp
@@ -2,7 +2,7 @@
 
 Bonus::Bonus(Sector* sector, QColor color) : Object(sector, color)
 {
-
+    setPos(0, 0);
 }
 
 void Bonus::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
@@ -13,9 +13,23 @@ void Bonus::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWi
     QPainterPath path;
     path.addRect(boundingRect());
 
-    QPoint topLeftPoint = boundingRect().topLeft().toPoint() + QPoint(SIZE, SIZE) / 4;
+    QPoint center = boundingRect().bottomRight().toPoint() / 2;
 
-    // Дописать
+    QPainterPath topLeftPath;
+    topLeftPath.addRect(QRect(boundingRect().topLeft().toPoint(), center + QPoint(-2, -2)));
+    path -= topLeftPath;
+
+    QPainterPath topRightPath;
+    topRightPath.addRect(QRect(boundingRect().topRight().toPoint(), center + QPoint(2, -2)));
+    path -= topRightPath;
+
+    QPainterPath bottomLeftPath;
+    bottomLeftPath.addRect(QRect(boundingRect().bottomLeft().toPoint(), center + QPoint(-2, 2)));
+    path -= bottomLeftPath;
+
+    QPainterPath bottomRightPath;
+    bottomRightPath.addRect(QRect(boundingRect().bottomRight().toPoint(), center + QPoint(2, 2)));
+    path -= bottomRightPath;
 
     painter->fillPath(path, color());
 }
