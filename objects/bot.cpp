5aa5efbbaa26b6a410fde2a6ff96c3ac1d11a98d
Bulat Gubaydullin <gubaydullin.bulat7@gmail.com>
2019-08-12 16:30:51 +0300

    Add bot states

--- objects/bot.cpp
+++ objects/bot.cpp
@@ -3,6 +3,18 @@
 Bot::Bot(Sector* sector) : Character(sector)
 {
     connect(animations.moveAnim, &QPropertyAnimation::finished, this, &Bot::slotFindCorrectMoveDir);
+
+    state = DO_NOTHING;
+}
+
+Bot::State Bot::getState() const
+{
+    return state;
+}
+
+void Bot::setState(State state)
+{
+    this->state = state;
 }
 
 void Bot::slotFindCorrectMoveDir()
@@ -12,37 +24,40 @@ void Bot::slotFindCorrectMoveDir()
         return;
     }
 
-    QPoint left  = position() + directions[Qt::Key_A];
-    QPoint right = position() + directions[Qt::Key_D];
-    QPoint up    = position() + directions[Qt::Key_W];
-    QPoint down  = position() + directions[Qt::Key_S];
-
-    QVector<QPoint> dirs = {left, right, up, down};
-    QVector<QPoint> validDirs;
-
-    for (int i = 0; i < dirs.size(); i++)
+    if (state == DO_NOTHING)
     {
-        QPoint nextPos = dirs[i];
+        QPoint left  = position() + directions[Qt::Key_A];
+        QPoint right = position() + directions[Qt::Key_D];
+        QPoint up    = position() + directions[Qt::Key_W];
+        QPoint down  = position() + directions[Qt::Key_S];
 
-        if (!insideScene(nextPos))
+        QVector<QPoint> dirs = {left, right, up, down};
+        QVector<QPoint> validDirs;
+
+        for (int i = 0; i < dirs.size(); i++)
         {
-            continue;
+            QPoint nextPos = dirs[i];
+
+            if (!insideScene(nextPos))
+            {
+                continue;
+            }
+
+            Sector* next = findNextSector(nextPos);
+            QPoint sectorPos = QPoint(nextPos.x() % next->width(), nextPos.y() % next->height());
+            bool roadInCurrentSector = sector()->cell(sectorPos.x(), sectorPos.y()).isRoad();
+            bool roadInNextSector = next->cell(sectorPos.x(), sectorPos.y()).isRoad();
+
+            bool validDirInCurrentSector = (sector() == next && roadInCurrentSector);
+            bool validDirInNextSector = (sector() != next && roadInNextSector);
+
+            if (validDirInCurrentSector || validDirInNextSector)
+            {
+                validDirs.push_back(nextPos);
+            }
         }
 
-        Sector* next = findNextSector(nextPos);
-        QPoint sectorPos = QPoint(nextPos.x() % next->width(), nextPos.y() % next->height());
-        bool roadInCurrentSector = sector()->cell(sectorPos.x(), sectorPos.y()).isRoad();
-        bool roadInNextSector = next->cell(sectorPos.x(), sectorPos.y()).isRoad();
-
-        bool validDirInCurrentSector = (sector() == next && roadInCurrentSector);
-        bool validDirInNextSector = (sector() != next && roadInNextSector);
-
-        if (validDirInCurrentSector || validDirInNextSector)
-        {
-            validDirs.push_back(nextPos);
-        }
+        QPoint nextPos = validDirs[qrand() % validDirs.size()];
+        move(nextPos);
     }
-
-    QPoint nextPos = validDirs[qrand() % validDirs.size()];
-    move(nextPos);
 }
--- objects/bot.h
+++ objects/bot.h
@@ -6,7 +6,20 @@
 class Bot : public Character
 {
 public:
+    enum State
+    {
+        ATTACK = 1,
+        RUN_AWAY,
+        FIND_BONUS,
+        DO_NOTHING
+    };
+private:
+    State state;
+public:
     Bot(Sector* sector);
+
+    State getState() const;
+    void setState(State state);
 private slots:
     void slotFindCorrectMoveDir() override;
 };
